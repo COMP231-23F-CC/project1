6c9e0f3b0b727eabd0a821e48e9774e1f7b5abdd
Broccoli <90281858+koma123451@users.noreply.github.com>
2023-11-23 22:17:53 -0500

    home page has been interacted with backend data

--- guest-side/client/src/components/featuredProperties/FeaturedProperties.jsx
+++ guest-side/client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const FeaturedProperties = () => {
   const { data, loading, error } = useFetch("http://localhost:8800/api/hotels?featured=true");  //如果在后面加上&limit=4 就是空的array，不显示数据，后端也看不到。 问题应该是在/controllers/hotels -getHotels 的limit问题。
- console.log(data)
+ console.log(data)                                                                              //这个url在后端也调不出来，mongodb里面确定已经有4个featured=true的hotel，不加&limit= 可以正常显示数据。
   return (
     <div className="fp">
       {loading ? (
