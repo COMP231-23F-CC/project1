f60ac04c690bb1f74a6271c971e27b790dbf7056
Shu Jin <sjin32@my.centennialcollege.ca>
2023-12-04 00:20:41 -0500

    project1 : booing front , update orderlist - delete fun

--- booking-frontend/src/pages/OrdersListPage.jsx
+++ booking-frontend/src/pages/OrdersListPage.jsx
@@ -1,59 +1,142 @@
-// src/pages/OrdersListPage.jsx
-
 import React, { useState, useEffect } from 'react';
-import { Typography, List, ListItem, ListItemText } from '@mui/material';
-import api from '../api/httpClient.js';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle
+} from '@mui/material';
+import api from '../api/httpClient';
+import dayjs from "dayjs";
 
-import OrdersTableComponent from '../components/OrdersTableComponent';
 
-const OrdersListPage = () => {
-    const [orders, setOrders] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState('');
+const BookingTable = () => {
+    const [bookings, setBookings] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [selectedId, setSelectedId] = useState(null);
 
     useEffect(() => {
-        const fetchOrders = async () => {
-            try {
-                const res = await api.get('/booking');
-                setOrders(res.data);
-                setLoading(false);
-            } catch (err) {
-                setError(err.message);
-                setLoading(false);
-            }
-        };
-
-        fetchOrders();
+        fetchBookings();
     }, []);
 
+    const fetchBookings = async () => {
+        try {
+            const response = await api.get('/Booking');
+            //show the data
+            console.log('fetchBookings response.data:', response.data);
 
+            setBookings(response.data);
 
-    const handleGuestInfoChange = (orderIndex, field, value) => {
-        const updatedOrders = orders.map((order, index) => {
-            if (index === orderIndex) {
-                return { ...order, [field]: value };
-            }
-            return order;
-        });
-        setOrders(updatedOrders);
+        } catch (err) {
+            console.error('Error fetching bookings:', err);
+        }
     };
 
+    const handleClickOpen = (id) => {
+        setOpen(true);
+        setSelectedId(id);
+    };
 
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-    if (loading) {
-        return <Typography>Loading orders...</Typography>;
-    }
+    const confirmDelete = () => {
+        deleteBooking(selectedId);
+    };
 
-    if (error) {
-        return <Typography color="error">Error: {error}</Typography>;
-    }
+    const deleteBooking = async (id) => {
+        try {
+            await api.delete(`/Booking/${id}`);
+            fetchBookings();
+            handleClose();
+        } catch (err) {
+            console.error('Error deleting booking:', err);
+        }
+    };
 
     return (
-        <div>
-            <Typography variant="h4">Your Orders</Typography>
-            <OrdersTableComponent orders={orders} onGuestInfoChange={handleGuestInfoChange} />
-        </div>
+        <>
+            <TableContainer component={Paper}>
+                <Table aria-label="booking table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Order ID</TableCell>
+
+
+                            <TableCell align="right">Check In </TableCell>
+                            <TableCell align="right">Check Out </TableCell>
+                            <TableCell align="right">Status</TableCell>
+                            <TableCell align="right">Room Number</TableCell>
+                            <TableCell align="right">Room Type</TableCell>
+                            <TableCell align="right">Total Price</TableCell>
+                            <TableCell align="right">Guest Name</TableCell>
+                            <TableCell align="right">Guest Phone</TableCell>
+                            <TableCell align="right">Remark</TableCell>
+                            <TableCell align="right">Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {bookings.map((booking) => (
+                            <TableRow key={booking.id}>
+                                <TableCell component="th" scope="row">
+                                    {booking.id}
+                                </TableCell>
+
+
+                                <TableCell align="right">{dayjs(booking.startDate).format('YYYY-MM-DD')}</TableCell>
+                                <TableCell align="right">{dayjs(booking.endDate).format('YYYY-MM-DD')}</TableCell>
+                                <TableCell align="right">{booking.status}</TableCell>
+                                <TableCell align="right">{booking.room.number}</TableCell>
+                                <TableCell align="right">{booking.room.type}</TableCell>
+                                <TableCell align="right">{booking.totalPrice}</TableCell>
+                                <TableCell align="right">{booking.guestName}</TableCell>
+                                <TableCell align="right">{booking.guestPhone}</TableCell>
+
+                                <TableCell align="right">{booking.remark}</TableCell>
+                                <TableCell align="right">
+                                    <Button variant="outlined" color="secondary" onClick={() => handleClickOpen(booking.id)}>
+                                        Delete
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            {/* Confirmation Dialog */}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Confirm Deletion"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Are you sure you want to delete this booking?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={confirmDelete} color="secondary" autoFocus>
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 };
 
-export default OrdersListPage;
+export default BookingTable;
